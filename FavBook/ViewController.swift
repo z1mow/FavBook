69955008a7afc7cbf51c651dc9d4dd5ec85f0daa
Şakir Yılmaz Öğüt <zimowcs@gmail.com>
2025-02-07 10:23:09 +0300

    Initial Commit

---
+++ FavBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavBook
+//
+//  Created by Şakir Yılmaz ÖĞÜT on 7.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
